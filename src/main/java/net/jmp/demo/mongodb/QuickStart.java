38c1a30c7bf14851ce8bb91d2946a4a8037a3435
Jonathan Parker <jonathanp62@gmail.com>
2023-11-23 08:20:06 -0500

    Added code to read multiple documents

--- src/main/java/net/jmp/demo/mongodb/QuickStart.java
+++ src/main/java/net/jmp/demo/mongodb/QuickStart.java
@@ -1,5 +1,6 @@
 package net.jmp.demo.mongodb;
 
+import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
 import org.bson.codecs.configuration.CodecProvider;
@@ -20,6 +21,8 @@ import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 final class QuickStart {
     private static final String MONGODB_URI = "mongodb://localhost:27017";
+    private static final String MONGO_DB_NAME = "local";
+    private static final String MONGO_COLLECTION_NAME = "demo";
 
     private final XLogger logger = new XLogger(LoggerFactory.getLogger(this.getClass().getName()));
 
@@ -28,38 +31,65 @@ final class QuickStart {
     }
 
     void run() {
-        this.document();
-        this.pojo();
-    }
-
-    private void document() {
         try (final var mongoClient = MongoClients.create(MONGODB_URI)) {
-            final var database = mongoClient.getDatabase("local");
-            final var collection = database.getCollection("demo");
-            final var doc = collection.find(eq("prodId", 100)).first();
-
-            if (doc != null) {
-                this.logger.info(doc.toJson());
-            } else {
-                this.logger.info("No matching document(s) found");
-            }
+            this.document(mongoClient);
+            this.pojo(mongoClient);
+            this.documents(mongoClient);
+            this.pojos(mongoClient);
         }
     }
 
-    private void pojo() {
+    private void document(final MongoClient mongoClient) {
+        final var database = mongoClient.getDatabase(MONGO_DB_NAME);
+        final var collection = database.getCollection(MONGO_COLLECTION_NAME);
+        final var doc = collection.find(eq("prodId", 100)).first();
+
+        if (doc != null) {
+            if (this.logger.isInfoEnabled())
+                this.logger.info(doc.toJson());
+        } else {
+            this.logger.info("No matching document(s) found");
+        }
+    }
+
+    private void pojo(final MongoClient mongoClient) {
         final CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
         final CodecRegistry pojoCodecRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
 
-        try (final var mongoClient = MongoClients.create(MONGODB_URI)) {
-            final var database = mongoClient.getDatabase("local").withCodecRegistry(pojoCodecRegistry);
-            final var collection = database.getCollection("demo", Demo.class);
-            final var demo = collection.find(eq("prodId", 100)).first();
+        final var database = mongoClient.getDatabase(MONGO_DB_NAME).withCodecRegistry(pojoCodecRegistry);
+        final var collection = database.getCollection(MONGO_COLLECTION_NAME, Demo.class);
+        final var demo = collection.find(eq("prodId", 100)).first();
 
-            if (demo != null) {
+        if (demo != null) {
+            if (this.logger.isInfoEnabled())
                 this.logger.info(demo.toString());
-            } else {
-                this.logger.info("No matching document(s) found");
-            }
+        } else {
+            this.logger.info("No matching document(s) found");
+        }
+    }
+
+    private void documents(final MongoClient mongoClient) {
+        final var database = mongoClient.getDatabase(MONGO_DB_NAME);
+        final var collection = database.getCollection(MONGO_COLLECTION_NAME);
+        final var docs = collection.find();
+
+        for (final var doc : docs) {
+            if (this.logger.isInfoEnabled())
+                this.logger.info(doc.toJson());
+        }
+    }
+
+    private void pojos(final MongoClient mongoClient) {
+        final CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
+        final CodecRegistry pojoCodecRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
+
+        final var database = mongoClient.getDatabase(MONGO_DB_NAME).withCodecRegistry(pojoCodecRegistry);
+        final var collection = database.getCollection(MONGO_COLLECTION_NAME, Demo.class);
+        final var demos = collection.find();
+
+        for (final var demo : demos) {
+            if (this.logger.isInfoEnabled())
+                this.logger.info(demo.toString());
         }
     }
 }
